fce63598bff8a41d9954fee16222ab7549e854f0
Alina Tsydzik <pronevich.alina@gmail.com>
2023-04-27 09:30:05 +0200

    fix: fix bugs according to rules ESLint in main.js

--- dist/main.js
+++ dist/main.js
@@ -1,32 +1,14 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/***/ (() => {
-
-eval("\n\n//# sourceURL=webpack://virtual-keyboard/./src/index.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
-/******/ 	
-/******/ })()
-;
+(() => { // webpackBootstrap
+// eslint-disable-next-line no-underscore-dangle, camelcase
+  const __webpack_modules__ = ({
+    './src/index.js':
+      (() => {
+        // eslint-disable-next-line no-eval
+        eval('\n\n//# sourceURL=webpack://virtual-keyboard/./src/index.js?');
+      }),
+  });
+  // eslint-disable-next-line camelcase, no-underscore-dangle, no-unused-vars
+  const __webpack_exports__ = {};
+  // eslint-disable-next-line camelcase
+  __webpack_modules__['./src/index.js']();
+})();
